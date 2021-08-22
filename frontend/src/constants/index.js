fd93e77c0e69cc5fc40a0f767a69050e05e7cb19
chlee1001 <chlee1001@naver.com>
2021-08-22 21:29:49 +0900

    deploy test

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://221.168.38.243:8080/";
+export const API_BASE_URL = "http://221.168.38.243:8080";
 export const ACCESS_TOKEN = "accessToken";
 
 export const OAUTH2_REDIRECT_URI = "http://221.168.38.243:3000/oauth2/redirect";
